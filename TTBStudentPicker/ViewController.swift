18fb62809ea1aa3665905a4ee4adf905dce4411f
Chase Hoselle <choselle@yahoo.com>
2019-04-20 19:36:58 -0500

    Started adding some core data code.  Working but needs to be changed to fit this app's needs.

--- TTBStudentPicker/ViewController.swift
+++ TTBStudentPicker/ViewController.swift
@@ -14,7 +14,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: context)
+        let newUser = NSManagedObject(entity: entity!, insertInto: context)
+        
+        
+        //newUser.setValue("1", forKey: "classId")
+        newUser.setValue("David", forKey: "name")
+        
+        do {
+            
+            try context.save()
+            
+        } catch {
+            
+            print("Failed saving")
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        //request.predicate = NSPredicate(format: "age = %@", "12")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "name") as! String)
+            }
+            
+        } catch {
+            
+            print("Failed")
+        }
     }
 }
 
